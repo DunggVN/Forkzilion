433639a4e5bd6054585539c8753a366bd15fa1c4
DunggVN <65992011+dunggvn@users.noreply.github.com>
2021-10-15 11:01:46 +0700

    Update purge.py

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -89,7 +89,7 @@ async def fastpurger(purg):
 async def purgeme(delme):
     """ For .purgeme, delete x count of your latest message."""
     message = delme.text
-    count = 0
+    count = int(message[9:])
     i = 1
 
     async for message in delme.client.iter_messages(delme.chat_id, from_user="me"):
